41f73b1081faea13a12b29799a338c2f837ef233
GustavoDiniz17 <gustavodinizdemorais@gmal.com>
2024-08-16 15:51:13 -0300

    Syntax correction

--- public/scripts.js
+++ public/scripts.js
@@ -15,7 +15,7 @@ const windElement = document.querySelector("#wind span");
 
 //Funções
 const getWeatherData = async(city) =>{
-    const apiWeatherURL = `https://api.openweathermap.org/data/3.0/onecall?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
     
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
