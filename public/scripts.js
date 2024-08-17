2fb43ec2ee89bc6cd6c7f4543d66729ae41daf6d
GustavoDiniz17 <gustavodinizdemorais@gmal.com>
2024-08-17 19:12:32 -0300

    API connected

--- public/scripts.js
+++ public/scripts.js
@@ -10,11 +10,13 @@ const tempElement = document.querySelector("#temperature span");
 const desceElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
+const weatherContainer = document.querySelector("#weather-data");
+
 //Funções
-const getWeatherData = async(city) =>{
+const getWeatherData = async (city) =>{
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
     
     const res = await fetch(apiWeatherURL);
@@ -28,6 +30,15 @@ const showWeatherData =  async (city) => {
     const data = await getWeatherData(city);
 
     cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    desceElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute('src',`	https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    const flagURL = getFlagURL(data.sys.country);
+    countryElement.setAttribute("src", flagURL);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}kh/h`;
+
+    weatherContainer.classList.remove('hide');
 };
 
 function getFlagURL(countryCode, style = "flat", size = "64") {
@@ -35,7 +46,7 @@ function getFlagURL(countryCode, style = "flat", size = "64") {
         .replace("{country_code}", countryCode)
         .replace("{style}", style)
         .replace("{size}", size);
-};
+}
 
 //Eventos
 searchBtn.addEventListener("click", async (e) => {
@@ -45,3 +56,12 @@ searchBtn.addEventListener("click", async (e) => {
 
     showWeatherData(city);
 });
+
+cityInput.addEventListener("keyup", (e) =>{
+
+    if(e.code === "Enter"){
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
+ });
